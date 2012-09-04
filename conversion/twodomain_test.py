d2dfefa2898cb1c6e9f3ac5cd801f967f6850fc9
Luke Olson <luke.olson@gmail.com>
2012-09-04 08:51:10 -0500

    starting over with this mess

--- conversion/twodomain_test.py
+++
@@ -1,6 +0,0 @@
-from dolfin import *
-mesh = Mesh('twodomain.xml')
-#subd = MeshFunction('uint',mesh,'twodomain_physical_region.xml')
-subd = MeshFunction('uint',mesh,'twodomain_physical_region.xml')
-out = File('test.pvd')
-out << subd
--- geo/testcubemerge.geo
+++
@@ -1,6 +0,0 @@
-Merge "cube.msh";
-ss[] = Surface "*";
-CreateTopology;
-Printf("%g",#ss[]);
-Surface Loop(1) = {ss[]};
-Volume(1)={1};
